c7dd741e700a51ce00950488764238d4eda18bb9
Vitor Campos <vitorcampos742@gmail.com>
2022-10-10 19:29:47 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    string1 = list(first_string.lower())
+    string2 = second_string.lower()
+    try:
+        for letter in string2:
+            string1.remove(letter)
+    except ValueError:
+        return False
+    if len(string1) == 0:
+        return True
+    else:
+        return False
