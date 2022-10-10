5fb8f93989930bde2c84264a621fbb0281df8f6e
Vitor Campos <vitorcampos742@gmail.com>
2022-10-10 16:40:09 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if not target_time and target_time != 0:
+        return None
+    count = 0
+    for aluno in permanence_period:
+        if type(aluno[0]) is not int or type(aluno[1]) is not int:
+            return None
+        if aluno[0] <= target_time <= aluno[1]:
+            count += 1
+    return count
