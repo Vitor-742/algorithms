20fde6a67e857fc498b7f91e6d7db062202892e2
Vitor Campos <vitorcampos742@gmail.com>
2022-11-08 15:07:43 -0300

    mundança de readme

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if (not word):
         return False
-    # caso o a palavra tenho chego ao final
     if high_index - low_index < 1:
         return True
     if word[low_index] != word[high_index]:
